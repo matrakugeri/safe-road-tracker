cf29929f8af377aae23e84a5d7e82cd2befff6bc
matrakugeri <matrakugeri@gmail.com>
2026-01-19 18:54:09 +0100

    defined routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'map',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/auth/containers/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/auth/containers/register/register').then((m) => m.Register),
+  },
+  {
+    path: '',
+    loadComponent: () => import('./layout/main-layout/main-layout').then((m) => m.MainLayout),
+    children: [
+      {
+        path: 'map',
+        loadComponent: () => import('./pages/map/containers/map/map').then((m) => m.Map),
+      },
+      {
+        path: 'report',
+        loadComponent: () =>
+          import('./pages/report/containers/report/report').then((m) => m.Report),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'map',
+  },
+];
