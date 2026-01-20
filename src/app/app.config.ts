36d150bf03df6cdc0d31ea8d877298b772074657
matrakugeri <matrakugeri@gmail.com>
2026-01-20 22:32:51 +0100

    small fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-
-registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
