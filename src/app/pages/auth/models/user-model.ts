116cccb33081d4b4ac951419c4607ca8c30c4f23
matrakugeri <matrakugeri@gmail.com>
2026-01-22 22:40:55 +0100

    small fix

--- src/app/pages/auth/models/user-model.ts
+++ src/app/pages/auth/models/user-model.ts
@@ -1,9 +1,15 @@
 export interface User {
+  _id: string;
   firstName: string;
   lastName: string;
   email: string;
-  password: string;
   role: 'admin' | 'user';
+}
+
+export interface UserResponse {
+  data: User;
+  token: string;
+  status: string;
 }
 
 export interface UserCredentials {
