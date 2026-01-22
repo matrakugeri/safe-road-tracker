7ed5fc0196b529a5c854aa6b460fa0f1b77ae9ff
matrakugeri <matrakugeri@gmail.com>
2026-01-22 22:41:14 +0100

    implement ngrx login actions

--- src/app/pages/auth/auth-store/auth.actions.ts
+++ src/app/pages/auth/auth-store/auth.actions.ts
@@ -1,7 +1,11 @@
 import { createAction, props } from '@ngrx/store';
+import { User } from '../models/user-model';
 
-export const login = createAction(`[User Login]`, props<{ email: string; password: string }>);
-export const loginSuccess = createAction(`[User Login Success]`);
-export const loginFailure = createAction(`[User Login Failure]`);
+export const login = createAction(`[User Login]`, props<{ email: string; password: string }>());
+export const loginSuccess = createAction(
+  `[User Login Success]`,
+  props<{ user: User; token: string }>(),
+);
+export const loginFailure = createAction(`[User Login Failure]`, props<{ error: string }>());
 
 export const register = createAction(`[User Register]`);
