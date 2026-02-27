c78134a1e39be8514d780984a74eb60d070707ad
matrakugeri <matrakugeri@gmail.com>
2026-02-27 21:49:33 +0100

    added different type of maps

---
+++ src/app/shared/leaflet-map/maps.ts
@@ -0,0 +1,7 @@
+export const Maps = {
+  darkMap: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+  mainMap: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  sateliteMap:
+    'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+  lightMap: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+};
